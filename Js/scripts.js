7fbe35dea3bda5ee89582b9b6dd219beb387a094
bano27 <thairukibe2798@gmail.com>
2020-10-13 04:53:36 +0300

    script file changed

--- Js/scripts.js
+++ Js/scripts.js
@@ -3,6 +3,6 @@ var a =	document.getElementById("myName").value;
 
 myForm.addEventListener("submit", (e) => {
 e.preventDefault();
-alert("+a+" + "	we have received your message. Thank you for reaching out to us");
+alert(a + "	we have received your message. Thank you for reaching out to us");
 
 });
